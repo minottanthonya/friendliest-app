959d8a3d22c6c78612ec786ec7407f26c8a5a630
Tony Minott <minottanthonya@gmail.com>
2017-01-24 14:10:42 -0500

    Finally FINALLY WORKING

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,89 @@
-var $friends = $('#friends');
-var $name = $('#name');
-var $occupation = $('#occupation');
-var $age = $('#age');
-
-var friendTemplate = "" +
-    "<li>" +
-
-    "<p><strong>Name:</strong> {{name}}</p>" +
-
-    "<p><strong>Occupation:</strong> {{occupation}}</p> " +
-
-    "<p><strong>Age:</strong> {{age}}</p> " +   
-
-    "</li>";
-
-//Adding a friend
+var $friends = $("#friends");
+var $name = $("#name");
+var $occupation = $("#occupation");
+var $age = $("#age");
+var friendTemplate =  "<tr>" + "<td>{{name}}</td>" + 
+     "<td>{{occupation}}</td>"  +
+     "<td>{{age}}</td>" + "<td><button id='{{id}}' class='remove btn btn-info'>X</button></td>" + "</tr>" + "<br>";
 function addFriend(friend){
     $friends.append(Mustache.render(friendTemplate, friend));
+
+    //emptys the input field
+    $name.val(""),
+    $occupation.val(""),
+    $age.val("")
 }
 
-
-
-
 $(document).ready(function(){
-    //alert("jQuery is working");
+    //  alert("it works")
 
     //GET Data request
     $.ajax({
-        type : 'GET',
-        url  : 'http://rest.learncode.academy/api/learncode/friends',
+        type : "GET",
+        url  : "http://rest.learncode.academy/api/learncode/friends",
 
-        success : function(friends){
+        //is success linked to ajax
+        success: function(friends){
+            friends.shift();
+            friends.shift();
             $.each(friends, function(i, friend){
                 addFriend(friend);
             });
+            $("#numfriend").append(friends.length);
         },
-    
+        //Error
         error : function(){
-            alert('Error loading friends');
+            alert("Error loading friends");
         }
-
     });
-        
-    //POST to Add a friend
-    $('#add-friend').on('click', function(){
 
+
+    //POST Data request
+    $("#add-friend").on("click", function(){
+
+        //creating new friend
         var friend = {
             name: $name.val(),
             occupation: $occupation.val(),
-            age: $age.val() 
+            age: $age.val()
         };
 
-        //AJAX POST FUNCTION
+        //adding friend
         $.ajax({
-            type : 'POST', 
-            url : 'http://rest.learncode.academy/api/learncode/friends',
-            data : friend,
-            success : function(newFriend){
-                addFriend(newFriend);
-            },
+            type: "POST",
+            url: "http://rest.learncode.academy/api/learncode/friends",
+            data: friend,
 
-            error  : function(){
-                alert('Error');
+            success: function(newFriend){
+                addFriend(newFriend);
+                $("#numfriend").append(friends.length);
+            },
+            error: function(){
+                alert("Error added friend");
             }
-        }); 
+
+        });
     });
 
-    //DELETE to dump 'em
+    //DELETE button 
+    $friends.delegate('.remove', 'click', function(){
 
+        var $li = $(this).closest('tr');
+        //AJAX DELETE Function - click the .remove class button and the id identifies what to delete
+        $.ajax({
+            type: 'DELETE',
+            url: 'http://rest.learncode.academy/api/learncode/friends/' + $(this).attr('id'),
+            success: function(){
+                $li.fadeOut(300, function(){
+                    $(this).remove();
+                });
+                $("#numfriend").append(friends.length);
+            },
+            error: function(){
+                alert("nope");
+            }
+        });
+    });
+    
+    $("#table").css("background-color", "powderblue");
 });
-
